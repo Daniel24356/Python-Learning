58b3ab8ce7d5a1325a585d7b54c74832b4e92b8f
Daniel24356 <igwedaniel128@gmail.com>
2025-08-22 04:06:09 -0700

    commented main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import datetime
-from message import get_user_name
-import platform
+# import datetime
+# from message import get_user_name
+# import platform
 
-# print(get_user_name("Daniel"))
-# pf = platform.system()
-# print(pf)
-# pf = dir(platform)
-# print(pf)
+# # print(get_user_name("Daniel"))
+# # pf = platform.system()
+# # print(pf)
+# # pf = dir(platform)
+# # print(pf)
 
-# print(user["age"])
-dt = datetime.datetime.now()
-print(dt.year)
-print(dt.hour)
-print(dt.second)
-print(dt.strftime("%d"))
+# # print(user["age"])
+# dt = datetime.datetime.now()
+# print(dt.year)
+# print(dt.hour)
+# print(dt.second)
+# print(dt.strftime("%d"))
 
