c6fd67d83fe2ed67e70b7e3ca22e0ca4652e8064
Daniel24356 <igwedaniel128@gmail.com>
2025-08-22 04:32:07 -0700

    added line61 in app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ print(first.endswith("t"))
 myvalue = True
 x = bool(False)
 print(type(x))
-# print(isinstance(myvalue, bool))
+print(isinstance(myvalue, bool))
 
 # comp_value = 5+3j
 # print(type(comp_value))
