5fe5295972cb869c05ec06749050fac469144d52
Daniel24356 <igwedaniel128@gmail.com>
2025-08-22 04:06:36 -0700

    commented message.py

--- message.py
+++ message.py
@@ -1,8 +1,8 @@
-def  get_user_name(name):
-     return f"Hello {name}!"
+# def  get_user_name(name):
+#      return f"Hello {name}!"
 
-user = {
-     "name":"Ridwon",
-     "age": 18,
-     "gender": "Male"
-}
+# user = {
+#      "name":"Ridwon",
+#      "age": 18,
+#      "gender": "Male"
+# }
