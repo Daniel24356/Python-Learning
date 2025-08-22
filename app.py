2af567eabdca7bf9e1489720b0352a8b70ccc418
Daniel24356 <igwedaniel128@gmail.com>
2025-08-22 04:27:02 -0700

    added line45 in app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ print(len(mutiline.lstrip()))
 print(len(mutiline.rstrip()))
 
 title = "menu".upper()
-# print(title.center(20,"="))
+print(title.center(20,"="))
 # print("Coffee".ljust(16,".") + '$1'.rjust(4))
 # print("Muffin".ljust(16,".") + '$2'.rjust(4))
 # print("Cheesecake".ljust(16,".") + '$4'.rjust(4))
