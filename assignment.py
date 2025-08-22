86c261c78f0bdc54a6b557ed34637ed717e6c44c
Daniel24356 <igwedaniel128@gmail.com>
2025-08-22 05:01:00 -0700

    completed digit classifier for assignment.py

--- assignment.py
+++ assignment.py
@@ -1,15 +1,15 @@
-# #1. Digit Classifier
-# def digitClassifier(number):
-#     if(number == 0):
-#         print("Zero")
-#     elif(number%2 == 0 and number%3 == 0):
-#         print("Even and divisible by 3")   
-#     elif(number%2 == 0):
-#         print("Even")
-#     else:
-#         print("Odd")
+#1. Digit Classifier
+def digitClassifier(number):
+    if(number == 0):
+        print("Zero")
+    elif(number%2 == 0 and number%3 == 0):
+        print("Even and divisible by 3")   
+    elif(number%2 == 0):
+        print("Even")
+    else:
+        print("Odd")
 
-# digitClassifier(9)        
+digitClassifier(9)        
 
 # # 2. Reverse Engineering
 # def reverseEngineering(number):
