739bcfb8a043aed27e62900aec23d49a57bad8b6
Daniel24356 <igwedaniel128@gmail.com>
2025-09-24 20:44:42 -0700

    removed the plot for matplot

--- matPlot.py
+++ matPlot.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 days = [1,2,3,4,5]
 sales = [50,60,55,70,65]
 
-plt.plot(days,sales,marker='o')
-plt.title("Sales over 5 days")
-plt.xlabel("Day")
-plt.ylabel("Unit Sold")
-plt.show()
+# plt.plot(days,sales,marker='o')
+# plt.title("Sales over 5 days")
+# plt.xlabel("Day")
+# plt.ylabel("Unit Sold")
+# plt.show()
 
 # products = ["Bread","Milk","Eggs"]
 # unit_sold = [300,220,180]
