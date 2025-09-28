cd2df94a0ff1720fb2e237579fd906a9eadf6f71
Daniel24356 <igwedaniel128@gmail.com>
2025-09-28 19:24:16 +0000

    reverted changes

--- main.py
+++ main.py
@@ -15,26 +15,26 @@
 # print(dt.second)
 # print(dt.strftime("%d"))
 
-# from bankAccount import BankAccount
-# from calculator import Calculator
-# from developer import Developer
-# from manager import Manager
+from bankAccount import BankAccount
+from calculator import Calculator
+from developer import Developer
+from manager import Manager
 
-# m = Manager("Mike", 30000, "HR")
-# d = Developer("Daniel", 50000, "Python")
+m = Manager("Mike", 30000, "HR")
+d = Developer("Daniel", 50000, "Python")
 
-# m.show_details()
-# print("Manager Bonus",m.calculate_bonus())
+m.show_details()
+print("Manager Bonus",m.calculate_bonus())
 
-# d.show_details()
-# print("Developers Bonus", d.calculate_bonus())
+d.show_details()
+print("Developers Bonus", d.calculate_bonus())
 
-# acc = BankAccount('Daniel', 5000)
-# acc.deposit(2000)
-# print(acc.get__balance())
-# print(acc.owner)
+acc = BankAccount('Daniel', 5000)
+acc.deposit(2000)
+print(acc.get__balance())
+print(acc.owner)
 
-# calc = Calculator()
-# print(calc.add(2,3))
-# print(calc.add(2,3,6,8))
-# print(calc.add(2,3,1,4,6,7,8))
+calc = Calculator()
+print(calc.add(2,3))
+print(calc.add(2,3,6,8))
+print(calc.add(2,3,1,4,6,7,8))
